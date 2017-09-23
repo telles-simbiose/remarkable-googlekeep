6c5ca5cced0922c4019ead5928f6b762793735bf
Rafael Telles <rafaelt@simbioseventures.com>
2017-09-23 12:37:45 -0300

    Decode HTML entities before processing markdown

--- src/content-scripts/inject.js
+++ src/content-scripts/inject.js
@@ -8,8 +8,9 @@
         var converter = window.markdownit();
         var contentEditables = document.querySelectorAll('div[contenteditable="false"].h1U9Be-YPqjbf');
 
-        for (var i = 0; i < contentEditables.length; i++) {        
-            var output = converter.render(contentEditables[i].innerHTML.replace(/<br>/g, '\n'));
+        for (var i = 0; i < contentEditables.length; i++) {
+            var contentText = htmlDecode(contentEditables[i].innerHTML.replace(/<br>/g, '\n'));
+            var output = converter.render(contentText);
             
             if (contentEditables[i].nextSibling.classList.contains('remarkable')) {
                 contentEditables[i].nextSibling.innerHTML = output;
@@ -18,5 +19,11 @@
             }
         }
         document.querySelector('.notes-container').addEventListener('DOMSubtreeModified', render, false);
+    }
+
+    function htmlDecode(input){
+        var e = document.createElement('div');
+        e.innerHTML = input;
+        return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
     }
 })();
